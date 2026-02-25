16159972c7832df19297682eeb346c4dde1d8394
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-02-25 09:45:42 +0200

    no endurance loss from dragging corpses

--- 42.13/media/lua/client/ZS_ZeroG.lua
+++ 42.13/media/lua/client/ZS_ZeroG.lua
@@ -5,20 +5,52 @@
 ZSpaceship = ZSpaceship or {}
 
 local ZERO_G_MAX_WEIGHT = 500
-local wasFallingInSpace = false
+
+local wasFallingInSpace = {}
+
+-- see IsoPlayer.java updateEndurance()
+local function compensateCorpseDraggingEndurance(player)
+    if not player:isMoving() then return end
+
+    local sneakMultiplier = 1.0
+    if player:isSneaking() then
+        sneakMultiplier = 1.5
+    end
+
+    local enduranceReduce = ZomboidGlobals.SprintingEnduranceReduce;
+    local enddelta = 1.4
+
+    if player:hasTrait(CharacterTrait.OVERWEIGHT) then enddelta = 2.9 end
+    if player:hasTrait(CharacterTrait.ATHLETIC)   then enddelta = 0.8 end
+
+    local enddelta2 = enddelta * 2.3 * player:getPacingMod() * player:getHyperthermiaMod()
+    local mod = 0.7
+    if player:hasTrait(CharacterTrait.ASTHMATIC)  then mod = 1.0 end
+
+    local delta = enduranceReduce * enddelta2 * 0.5 * mod * GameTime.instance:getMultiplier() * sneakMultiplier
+    if delta <= 0 or delta > 25 then return end -- something calculated wrong
+
+    player:getStats():add(CharacterStat.ENDURANCE, delta)
+end
 
 local function updateSpacePhysics()
     local player = getPlayer()
     if not player then return end
     
+    local pid = player:getID() -- or getPlayerNum() ?
     local inSpace = zsInSpace(player)
     
     if not inSpace then
-        if wasFallingInSpace then
+        if wasFallingInSpace[pid] then
             player:setbClimbing(false)
-            wasFallingInSpace = false
+            wasFallingInSpace[pid] = nil
         end
         return
+    end
+
+    -- less endurance reduce when dragging corpses
+    if player.isDraggingCorpse and player:isDraggingCorpse() then
+        compensateCorpseDraggingEndurance(player)
     end
     
     -- Zero gravity: no heavy load moodle
@@ -33,13 +65,13 @@ local function updateSpacePhysics()
                       player:getCurrentState() == PlayerFallingState.instance()
     
     if isFalling then
-        if not wasFallingInSpace then
+        if not wasFallingInSpace[pid] then
             player:setbClimbing(true)
-            wasFallingInSpace = true
+            wasFallingInSpace[pid] = true
         end
-    elseif wasFallingInSpace then
+    elseif wasFallingInSpace[pid] then
         player:setbClimbing(false)
-        wasFallingInSpace = false
+        wasFallingInSpace[pid] = nil
     end
 end
 
