c237c885dd391908f379c581f0f2d5330f8eb3d6
Andrey "Zed" Zaikin <zed.0xff@gmail.com>
2026-03-21 09:42:38 +0200

    verbatim

--- tools/lib/GameVersion.rb
+++ tools/lib/GameVersion.rb
@@ -1,4 +1,4 @@
-class GameVersion
+class GameVersion # verbatim
   attr_reader :m_major, :m_minor, :m_suffix
 
   def initialize(major, minor, suffix = nil)
@@ -11,19 +11,16 @@ class GameVersion
     "#{@m_major}.#{@m_minor}#{@m_suffix}"
   end
 
-  def <=>(other)
-    return @m_major  <=> other.m_major  if @m_major  != other.m_major
-    return @m_minor  <=> other.m_minor  if @m_minor  != other.m_minor
-    return @m_suffix <=> other.m_suffix if @m_suffix != other.m_suffix
-    0
+  def getInt()
+    @m_major * 1000 + @m_minor
   end
 
-  def >(other)
-    (self <=> other) > 0
+  def isGreaterThan(other)
+    getInt() > other.getInt()
   end
 
+  # non-verbatim
   B41 = GameVersion.new(41, 0)
   B42 = GameVersion.new(42, 0)
-
   DEFAULT = B42
 end
